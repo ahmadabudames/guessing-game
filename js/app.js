a33e20f8770af71f82431d4653450b1cd3256497
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-03-29 17:05:16 +0300

    new file

---
+++ js/app.js
@@ -0,0 +1,114 @@
+'use strict';
+
+let ready=prompt('are you ready?');
+
+switch(ready.toLowerCase()){
+    case 'yes':
+    // console.log('lets start');
+   alert('lets start');
+    break;
+    case 'y':
+    // console.log('lets start');
+   alert('lets start');
+    break;
+    case 'no':
+        // console.log('take your time to be ready');
+        alert('take your time to be ready');
+        break;
+        case 'n':
+            // console.log('take your time to be ready');
+            alert('take your time to be ready');
+}
+
+let enthusiastic=prompt('are you enthusiastic?');
+
+switch(enthusiastic.toLowerCase()){
+    case 'yes':
+        // console.log('nice to hear that');
+    alert('nice to hear that');
+    break;
+    case 'y':
+        // console.log('nice to hear that');('nice to hear that');
+        alert('nice to hear that');
+    break;
+    case 'no':
+        //  console.log('oh you looked like you had difficult day');
+        alert('oh you looked like you had difficult day');
+        break;
+        case 'n':
+            // console.log('oh you looked like you had difficult day');
+           alert('oh you looked like you had difficult day');
+}
+
+
+let feelgood=prompt('are you feel good?');
+
+switch(feelgood.toLowerCase()){
+    case 'yes':
+        // console.log('nice to hear that');
+       alert('nice to hear that');
+    break;
+    case 'y':
+    // console.log('nice to hear that');
+    alert('nice to hear that');
+    break;
+    case 'no':
+        // console.log('sorry for hear that');
+       alert('sorry for hear that');
+        break;
+        case 'n':
+           
+            // console.log('sorry for hear that');
+           alert('sorry for hear that');
+}
+
+
+let experience=prompt('are you have any previous experience?');
+
+switch(experience.toLowerCase()){
+    case 'yes':
+    // console.log('we need you to be with us');
+   alert('we need you to be with us');
+    break;
+    case 'y':
+    // console.log('we need you to be with us');
+    alert('we need you to be with us');
+    break;
+    case 'no':
+       
+        // console.log('better you have ');
+       alert('better you have ');
+        break;
+        case 'n':
+            // console.log('better you have');
+            alert('better you have ');
+}
+
+
+let withus=prompt('do you accept to be with us?');
+
+switch(withus.toLowerCase()){
+    case 'yes':
+    // console.log('welcome to be with us');
+    alert('welcome to be with us');
+    break;
+    case 'y':
+    // console.log('welcome to be with us');
+    alert('welcome to be with us');
+    break;
+    case 'no':
+        // console.log('oh sorry, nice to meet you ');
+        alert('oh sorry, nice to meet you ');
+        break;
+        case 'n':
+//             console.log('oh sorry, nice to meet you ');
+alert('oh sorry, nice to meet you ');
+}
+
+
+
+
+
+
+
+
