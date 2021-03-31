6d77b32ff8531a9ce186eee228d5c34da98e3f32
bahaamq <bahaa.mq@gmail.com>
2021-03-31 16:01:04 +0300

    Adding-function-for-q7

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+
 let score = 0;
 let username = prompt('what is your name?');
 while (!username) {
@@ -33,158 +34,159 @@ function questionOne() {
 }
 
 //Question 2
-function questionTwo()
-{
-let enthusiastic = prompt('do i enthusiastic?');
+function questionTwo() {
+    let enthusiastic = prompt('do i enthusiastic?');
 
-switch (enthusiastic.toLowerCase()) {
-    case 'yes':
-        // console.log('nice to hear that');
-        alert('nice to hear that');
-        score++;
-        break;
-    case 'y':
-        // console.log('nice to hear that');('nice to hear that');
-        alert('nice to hear that');
-        score++;
-        break;
-    case 'no':
-        //  console.log('oh you looked like you had difficult day');
-        alert('oh you looked like you had difficult day');
-        break;
-    case 'n':
-        // console.log('oh you looked like you had difficult day');
-        alert('oh you looked like you had difficult day');
-}
+    switch (enthusiastic.toLowerCase()) {
+        case 'yes':
+            // console.log('nice to hear that');
+            alert('nice to hear that');
+            score++;
+            break;
+        case 'y':
+            // console.log('nice to hear that');('nice to hear that');
+            alert('nice to hear that');
+            score++;
+            break;
+        case 'no':
+            //  console.log('oh you looked like you had difficult day');
+            alert('oh you looked like you had difficult day');
+            break;
+        case 'n':
+            // console.log('oh you looked like you had difficult day');
+            alert('oh you looked like you had difficult day');
+    }
 }
 
 //Question 3
-function questionThree(){
-let feelgood = prompt('do i  feel good?');
+function questionThree() {
+    let feelgood = prompt('do i  feel good?');
 
-switch (feelgood.toLowerCase()) {
-    case 'yes':
-        // console.log('nice to hear that');
-        alert('nice to hear that');
-        score++;
-        break;
-    case 'y':
-        // console.log('nice to hear that');
-        alert('nice to hear that');
-        score++;
-        break;
-    case 'no':
-        // console.log('sorry for hear that');
-        alert('sorry for hear that');
-        break;
-    case 'n':
+    switch (feelgood.toLowerCase()) {
+        case 'yes':
+            // console.log('nice to hear that');
+            alert('nice to hear that');
+            score++;
+            break;
+        case 'y':
+            // console.log('nice to hear that');
+            alert('nice to hear that');
+            score++;
+            break;
+        case 'no':
+            // console.log('sorry for hear that');
+            alert('sorry for hear that');
+            break;
+        case 'n':
 
-        // console.log('sorry for hear that');
-        alert('sorry for hear that');
-}
+            // console.log('sorry for hear that');
+            alert('sorry for hear that');
+    }
 }
 
 // Question 4
-function questionFour(){
+function questionFour() {
 
-let experience = prompt('do i have any previous experience?');
+    let experience = prompt('do i have any previous experience?');
 
-switch (experience.toLowerCase()) {
-    case 'yes':
-        // console.log('we need you to be with us');
-        alert('we need you');
-        score++;
-        break;
-    case 'y':
-        // console.log('we need you to be with us');
-        alert('we need you');
-        score++;
-        break;
-    case 'no':
+    switch (experience.toLowerCase()) {
+        case 'yes':
+            // console.log('we need you to be with us');
+            alert('we need you');
+            score++;
+            break;
+        case 'y':
+            // console.log('we need you to be with us');
+            alert('we need you');
+            score++;
+            break;
+        case 'no':
 
-        // console.log('better you have ');
-        alert('better you have ');
-        break;
-    case 'n':
-        // console.log('better you have');
-        alert('better you have ');
-}
+            // console.log('better you have ');
+            alert('better you have ');
+            break;
+        case 'n':
+            // console.log('better you have');
+            alert('better you have ');
+    }
 }
 //Question 5
 
-function questionFive()
-{
-let withus = prompt('do i like blue color?');
+function questionFive() {
+    let withus = prompt('do i like blue color?');
 
-switch (withus.toLowerCase()) {
-    case 'yes':
-        // console.log('nice color');
-        alert('nice color');
-        score++;
-        break;
-    case 'y':
-        // console.log('nice color');
-        alert('nice color');
-        score++;
-        break;
-    case 'no':
-        // console.log('i dont like it');
-        alert('i dont like it ');
-        break;
-    case 'n':
-        //             console.log('i dont like it ');
-        alert('i dont like it ');
+    switch (withus.toLowerCase()) {
+        case 'yes':
+            // console.log('nice color');
+            alert('nice color');
+            score++;
+            break;
+        case 'y':
+            // console.log('nice color');
+            alert('nice color');
+            score++;
+            break;
+        case 'no':
+            // console.log('i dont like it');
+            alert('i dont like it ');
+            break;
+        case 'n':
+            //             console.log('i dont like it ');
+            alert('i dont like it ');
 
-}
+
+    }
 }
 
 //Question 6
 
-function questionSix()
-{
-let bMonth = prompt('which month i was born?');
+function questionSix() {
+    let bMonth = prompt('which month i was born?');
 
-for (let q = 0; q < 4; q++) {
-    if (bMonth === 11) {
-        alert('correct answer')
-        score++
-        break;
-    }
+    for (let q = 0; q < 4; q++) {
+        if (bMonth === 11) {
+            alert('correct answer')
+            score++
+            break;
+        }
 
-    else if (bMonth > 11) {
-        bMonth = prompt('too high,try again');
+        else if (bMonth > 11) {
+            bMonth = prompt('too high,try again');
 
-    } else if (bMonth < 11) {
-        bMonth = prompt('too low,try again');
-    }
-    else {
-        alert('the correct answer is 11')
-        break;
+        } else if (bMonth < 11) {
+            bMonth = prompt('too low,try again');
+        }
+        else {
+            alert('the correct answer is 11')
+            break;
+        }
     }
 }
+
+//Question 7
+function questionSeven() {
+    let color = 0;
+    let arrayofColor = ['red', 'black', 'blue'];
+    let attempts = 6;
+
+    hasAttempts: while (attempts) {
+        color = prompt('which color i like it?');
+        attempts = attempts - 1;
+        for (let i = 0; i < arrayofColor.length; i++) {
+
+            if (color === arrayofColor[i]) {
+                alert('correct here the answer' + arrayofColor);
+                break hasAttempts;
+            }
+        }
+        alert('sorry incorrect answer');
+    }
+    if (!attempts) {
+        alert('sorry your chance its over , here are the answer' + arrayofColor);
+    }
+
+
 }
-// let color = 0;
-// let arrayofColor = ['red', 'black', 'blue'];
-// let attempts = 6;
-
-// hasAttempts: while (attempts) {
-//     color = prompt('which color i like it?');
-//     attempts = attempts - 1;
-//     for (let i = 0; i < arrayofColor.length; i++) {
-
-//         if (color === arrayofColor[i]) {
-//             alert('correct here the answer' + arrayofColor);
-//             break hasAttempts;
-//         }
-//     }
-//     alert('sorry incorrect answer');
-// }
-// if (!attempts) {
-//     alert('sorry your chance its over , here are the answer' + arrayofColor);
-// }
-
-
-
 
 
 //Calling 
@@ -195,3 +197,4 @@ questionThree();
 questionFour();
 questionFive();
 questionSix();
+questionSeven();
